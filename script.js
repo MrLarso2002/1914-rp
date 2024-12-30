90b6d10c979089bed92cbd9b59ce7ab5158de308
LarsIsHere <lars-private@hotmail.com>
2024-12-30 20:13:38 +0100

    yay

--- script.js
+++ script.js
@@ -1,35 +1,56 @@
 
-let admin = 'url';
+
+let start_gamee = 1735776000*1000;
+let start =				1735776000*1000;
+let now = Date.now();
+const monthRanges = {
+    0: "October - December",
+    3: "January - March",
+    6: "April - June",
+    9: "July - September"
+};
+
+let starting_year = 1908;
 
 document.addEventListener('DOMContentLoaded', async () => {
-    try {
-        const response = await fetch(admin);
-        if (!response.ok) {
-          throw new Error(`Response status: ${response.status}`);
-        }
-    
-        const json = await response.json();
-        console.log(json);
+    if (now > start) {
+        // real life days
+        const days = Math.floor(((now - start) / 1000) / 86400);
+        console.log("real life days elapsed:" + days);
 
-        json.games.forEach(game => {
-            const element = document.createElement('div');
-            element.classList.add('game_button');
-            element.innerHTML = game.name;
-            if (game.priority) {
-                element.classList.add('game_button-active');
+        if (!days == 0) {
+            let years = days / 4;
+            console.log(years);
+            if (days % 4 === 0) {
+                console.log("e");
+                years = years - 1;
             }
-            element.addEventListener('click', () => {
-                document.querySelector('.games').children.forEach(child => {
-                    if (child.classList.contains('game_button-active')) {
-                        child.classList.remove('game_button-active');
-                    }
-                })
-                element.classList.add('game_button-active');
-            });
-            document.querySelector('.games').appendChild(element);
-        });
+            const current_year = starting_year + years;
+    
+            // ingame years
+            console.log(Math.floor(current_year))
+            console.log("years elapsed:" + years)
+    
+    
+            // months elapsed
+            const months_elapsed = (years - Math.floor(years).toFixed(2)) * 12;
+            console.log(months_elapsed);
+            const years_elapsed = Math.floor(years);
+            console.log(years_elapsed);
+            
+            console.log(monthRanges[months_elapsed])
 
-      } catch (error) {
-        console.error(error.message);
+            document.getElementById('date').innerHTML = "Day " + days + ", " + monthRanges[months_elapsed] + " " + current_year;
+            document.getElementById('years_elapsed').innerHTML = `${years_elapsed} ${years_elapsed === 1 ? "year" : "years"}`;
+            document.getElementById('months_elapsed').innerHTML = `${months_elapsed} ${months_elapsed === 1 ? "month" : "months"}`
+        } else {
+            document.getElementById('date').innerHTML = "Day 0, purgatory";
+        }
+
+      } else {
+        console.log('Not yet started');
+        document.getElementById('date').innerHTML = "The game hasn't begun yet";
       }
 });
+
+
